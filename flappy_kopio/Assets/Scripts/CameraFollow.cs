af8569fafcd19a6159493748bd2dbe96c421d399
kerttuli <kerttulr@metropolia.fi>
2021-02-25 19:17:25 +0200

    flappy

---
+++ flappy_kopio/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    // The Target
+    public Transform target;
+
+    // LateUpdate is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x,
+                                     transform.position.y,
+                                     transform.position.z);
+    }
+}
